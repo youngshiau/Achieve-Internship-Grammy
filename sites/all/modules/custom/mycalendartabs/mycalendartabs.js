36b28d27a536800a8a81ff4125395b75c25dd67c
Young Shiau <youngshiau@gmail.com>
2015-03-23 10:12:58 -0700

    Finished Javascript for MyCalendarTabs.

--- sites/all/modules/custom/mycalendartabs/mycalendartabs.js
+++ sites/all/modules/custom/mycalendartabs/mycalendartabs.js
@@ -1,79 +1,73 @@
-/*
-  Basic logic:
- 
- 
-  document.onload = function() {
-    var month = $('#calendar-month');
+ /**
+  * @file Javascript for switching between views.
+  */
+(function ($) {
+/**
+ * Attaches a click handler to the Month, Year, and Upcoming buttons.
+ *
+ * @property {function} attach
+ *   Attaches a click handler to the Month, Year, and Upcoming buttons to call
+ *   toggleDisplay().
+ */
+Drupal.behaviors.myCalendarTabs = {
+  attach: function(context, settings) {
+    var month = $('#mycalendartabs-month');
+    var year = $('#mycalendartabs-year');
+    var upcoming = $('#mycalendartabs-upcoming');
+
+    // Attach a click handler to the Month button.
     month.click(function() {
-      month.toggleDisplay;
+      toggleDisplay('month');
     });
-    
-    var year = $('#calendar-year');
+
+    // Attach a click handler to the Year button.
     year.click(function() {
-      year.toggleDisplay;
-    });   
-     
-    var upcoming = $('#calendar-upcoming');
+      toggleDisplay('year');
+    });
+
+    // Attach a click handler to the Upcoming button.
     upcoming.click(function() {
-      upcoming.toggleDisplay;
+      toggleDisplay('upcoming');
     });
   }
-  function toggleDisplay() {
-    var prevSelected = $('#mycalendartabs > .selected');
-    var newSelected = $(this); // check syntax and stuffs on this
-    prevSelected.removeClass('selected');
-    prevSelected.hide();
-    newSelected.addClass('selected');
-    newSelected.show();
-  } 
+};
+/**
+ * Toggles the Month, Year, and Upcoming views of the Calendar page based
+ * on which button was clicked.
+ *
+ * @param {string} button
+ *   The name of the button that was clicked.
  */
- 
-(function ($) {
-  Drupal.behaviors.exampleModule = {
-    attach: function(context, settings) {
-      var month = $('#mycalendartabs-month');
-      var year = $('#mycalendartabs-year');
-      var upcoming = $('#mycalendartabs-upcoming');
-      
-      // attach a click handler to the Month button 
-      month.click(function() {
-        toggleDisplay('month');
-      });
-      
-      // attach a click handler to the Year button
-      year.click(function() {
-        toggleDisplay('year');
-      });
-      
-      // attach a click handler to the Upcoming button
-      upcoming.click(function() {
-        toggleDisplay('upcoming');
-      });
-      
-      function toggleDisplay(button) {
-        var prevSelected = $('#mycalendartabs-tabs > .selected');
-        var newSelected = $('#mycalendartabs-' + button);
-        prevSelected.removeClass('selected');
-        newSelected.addClass('selected');
-        
-        switch(button) {
-          case 'month':
-            $('#calendar-month').show();
-            $('#calendar-year').hide();
-            $('#calendar-upcoming').hide();
-            break;
-          case 'year':
-            $('#calendar-year').show();
-            $('#calendar-month').hide();
-            $('#calendar-upcoming').hide();
-            break;
-          case 'upcoming':
-            $('#calendar-upcoming').show();
-            $('#calendar-month').hide();
-            $('#calendar-year').hide();
-            break;
-        }
-      }
-    }
-  };
-})(jQuery);
+function toggleDisplay(button) {
+
+  // Removes the 'selected' class from the previously clicked button and
+  // adds it to the newly clicked button.
+  var prevSelected = $('#mycalendartabs-tabs > .selected');
+  var newSelected = $('#mycalendartabs-' + button);
+  prevSelected.removeClass('selected');
+  newSelected.addClass('selected');
+
+  // Hides and displays different sections of the calendar page depending
+  // on the 'button' parameter.
+  switch(button) {
+    case 'month':
+      $('#calendar-month').show();
+      $('#calendar-year').hide();
+      $('#calendar-upcoming').hide();
+      break;
+    case 'year':
+      $('#calendar-year').show();
+      $('#calendar-month').hide();
+      $('#calendar-upcoming').hide();
+      break;
+    case 'upcoming':
+      $('#calendar-upcoming').show();
+      $('#calendar-month').hide();
+      $('#calendar-year').hide();
+      break;
+
+    default:
+      break;
+  }
+}
+}(jQuery));
