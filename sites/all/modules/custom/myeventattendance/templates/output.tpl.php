e68c31696c6048ffa0b043f77ba38ccfae650ae2
Young Shiau <youngshiau@gmail.com>
2015-03-24 14:59:57 -0700

    Added t() to user-visible output and finished commenting mycalendartabs and myeventattendance.

--- sites/all/modules/custom/myeventattendance/templates/output.tpl.php
+++ sites/all/modules/custom/myeventattendance/templates/output.tpl.php
@@ -1,5 +1,2 @@
-<?php
-drupal_add_js(drupal_get_path('module', 'myeventattendance') . '/myeventattendance.js');  
-?>
 <div id="myeventattendance-status"><?php print $status; ?></div>
 <div id="myeventattendance-buttons"><?php print $willAttend . $willNotAttend; ?></div>
